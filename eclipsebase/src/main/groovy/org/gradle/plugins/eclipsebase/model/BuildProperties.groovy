7ed1d30bbd1ca4e9ca91c369282d762ab8e1af8d
Markus Oley <markus.oley@vsa.de>
2014-12-19 09:27:55 +0100

    Bugfix reading build properties under windows

--- eclipsebase/src/main/groovy/org/gradle/plugins/eclipsebase/model/BuildProperties.groovy
+++ eclipsebase/src/main/groovy/org/gradle/plugins/eclipsebase/model/BuildProperties.groovy
@@ -10,10 +10,13 @@ import groovy.util.logging.Slf4j
  * To change this template use File | Settings | File Templates.
  */
 @Slf4j
-public class BuildProperties extends AbstractDescriptionFile{
+public class BuildProperties {
 
     Collection <String> binIncludes = new ArrayList<String>()
 
+    protected HashMap<String, Collection<String>> chapters = new HashMap<String, Collection<String>>()
+
+    protected File file
     /**
      * constructor
      * @param buildpropertiesFile   file
@@ -21,7 +24,7 @@ public class BuildProperties extends AbstractDescriptionFile{
     public BuildProperties (final File buildpropertiesFile) {
         this.file = buildpropertiesFile
         if (buildpropertiesFile.exists()) {
-          readChapters(new FileInputStream(buildpropertiesFile), "=")
+          readChapters(buildpropertiesFile.text.denormalize(), "=")
           readBinIncludes()
         }
     }
@@ -35,6 +38,46 @@ public class BuildProperties extends AbstractDescriptionFile{
         for (String nextBundlestring: bundlestrings) {
             if (! nextBundlestring.equals("."))
               binIncludes.add(nextBundlestring)
+        }
+    }
+
+
+
+    /**
+     * reads chapter of file
+     * @param fileContent      content of file as denormalized string
+     * @param divider   divider to split chapter from content
+     */
+    private void readChapters (String fileContent, final String divider) {
+        log.info("Read content " + fileContent)
+        Collection<String> lines = fileContent.split(System.lineSeparator())
+        for (String nextLine: lines) {
+            log.info("Reading line $nextLine from $file.absolutePath")
+        }
+
+        String currentChapter = null
+        for (String next: lines) {
+
+            if (next.trim().startsWith("#") || next.trim().isEmpty())
+                continue
+
+            String content = next
+            int indexSeparator = next.indexOf(divider)
+            log.info("Reading line " + next + " with divider on position " + indexSeparator)
+            if (indexSeparator > 0 && ! new Character(next.charAt(0)).isWhitespace()) {
+                currentChapter = next.substring(0, indexSeparator).trim()
+                log.info("set current chapter to " + currentChapter)
+                chapters.put(currentChapter, new ArrayList<String>())
+                content = next.substring(indexSeparator + 1, next.length())
+                log.info("set content to " + content)
+            }
+
+            content = content.replace(",", "").replace("\\", "").trim()
+            log.info("set content to " + content)
+
+            Collection <String> contentOfChapter = chapters.get(currentChapter)
+            contentOfChapter.add(content)
+            log.info("add content " + content + " to chapter " + currentChapter)
         }
     }
 
