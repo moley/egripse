3bbcd81fb0e0e6fd0b54c0234f38e5d0a5e268e3
OleyMa <markus.oley@vsa.de>
2020-03-26 15:25:56 +0100

    gradle6

--- eclipsebase/src/main/groovy/org/gradle/plugins/eclipsebase/model/BundlesInfo.groovy
+++ eclipsebase/src/main/groovy/org/gradle/plugins/eclipsebase/model/BundlesInfo.groovy
@@ -1,9 +1,8 @@
 package org.gradle.plugins.eclipsebase.model
 
 
-import org.gradle.internal.impldep.com.google.common.io.Files
-
 import java.nio.charset.Charset
+import java.nio.file.Files
 
 /**
  * reads the file build/oomph-ide.app/Contents/Eclipse/configuration/org.eclipse.equinox.org.eclipse.equinox.org.eclipse.equinox.simpleconfigurator/bundles.info
@@ -14,7 +13,7 @@ class BundlesInfo {
   private List <BundlesInfoEntry> bundlesInfoEntries = new ArrayList<BundlesInfoEntry>()
 
   public BundlesInfo (final File file) {
-    Collection<String> lines = Files.readLines(file, Charset.defaultCharset())
+    Collection<String> lines = Files.readAllLines(file, Charset.defaultCharset())
     for (String next: lines) {
       if (! next.startsWith("#")) {
         String [] tokens = next.split(",")
